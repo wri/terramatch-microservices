c0b94b16ddb5c819bfe84e35e41abfda61615bdd
Nathan Curtis <nathan.curtis@gmail.com>
2024-09-11 14:15:30 -0700

    [TM-1270] Rename UserService -> user-service

--- apps/user-service-e2e/jest.config.ts
+++ apps/user-service-e2e/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-  displayName: 'UserService-e2e',
+  displayName: 'user-service-e2e',
   preset: '../../jest.preset.js',
   globalSetup: '<rootDir>/src/support/global-setup.ts',
   globalTeardown: '<rootDir>/src/support/global-teardown.ts',
@@ -15,5 +15,5 @@ export default {
     ],
   },
   moduleFileExtensions: ['ts', 'js', 'html'],
-  coverageDirectory: '../../coverage/UserService-e2e',
+  coverageDirectory: '../../coverage/user-service-e2e',
 };
--- apps/user-service/webpack.config.js
+++ apps/user-service/webpack.config.js
@@ -3,7 +3,7 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, '../../dist/apps/UserService'),
+    path: join(__dirname, '../../dist/apps/user-service'),
   },
   plugins: [
     new NxAppWebpackPlugin({

3a4056fc3122a2cd9d9152abdebdc8ada095c887
Nathan Curtis <nathan.curtis@gmail.com>
2024-09-11 15:51:46 -0700

    [TM-1270] Restructure the api-gateway to be an NX app.

--- apps/user-service/src/main.ts
+++ apps/user-service/src/main.ts
@@ -1,8 +1,3 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 
@@ -10,12 +5,12 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const globalPrefix = 'api';
+  const globalPrefix = 'user-service';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT ?? 4010;
   await app.listen(port);
   Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
+    `User Service is running on: http://localhost:${port}/${globalPrefix}`
   );
 }
 

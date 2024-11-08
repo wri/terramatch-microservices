c2bbacfa82d4c02f6104761a9beb8691beaefd5b
Nathan Curtis <nathan.curtis@gmail.com>
2024-11-07 21:18:37 -0800

    [TM-1451] Implement API key authorization.

--- libs/common/src/lib/guards/auth.guard.ts
+++ libs/common/src/lib/guards/auth.guard.ts
@@ -1,13 +1,9 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  Injectable, SetMetadata,
-  UnauthorizedException
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
-import { Reflector } from '@nestjs/core';
+import { CanActivate, ExecutionContext, Injectable, SetMetadata, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+import { Reflector } from "@nestjs/core";
+import { User } from "@terramatch-microservices/database/entities";
 
-const NO_BEARER_AUTH = 'noBearerAuth';
+const NO_BEARER_AUTH = "noBearerAuth";
 export const NoBearerAuth = SetMetadata(NO_BEARER_AUTH, true);
 
 @Injectable()
@@ -17,22 +13,34 @@ export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const skipAuth = this.reflector.getAllAndOverride<boolean>(NO_BEARER_AUTH, [
       context.getHandler(),
-      context.getClass(),
+      context.getClass()
     ]);
     if (skipAuth) return true;
 
     const request = context.switchToHttp().getRequest();
-    const [type, token] = request.headers.authorization?.split(' ') ?? [];
-    if (type !== 'Bearer' || token == null) throw new UnauthorizedException();
+    const [type, token] = request.headers.authorization?.split(" ") ?? [];
+    if (type !== "Bearer" || token == null) throw new UnauthorizedException();
 
+    const userId = (await this.getJwtUserId(token)) ?? (await this.getApiKeyUserId(token));
+    if (userId == null) throw new UnauthorizedException();
+
+    // Most requests won't need the actual user object; instead the roles and permissions
+    // are fetched from other (smaller) tables, and only the user id is needed.
+    request.authenticatedUserId = userId;
+    return true;
+  }
+
+  private async getJwtUserId(token: string) {
     try {
       const { sub } = await this.jwtService.verifyAsync(token);
-      // Most requests won't need the actual user object; instead the roles and permissions
-      // are fetched from other (smaller) tables, and only the user id is needed.
-      request.authenticatedUserId = sub;
-      return true;
+      return sub;
     } catch {
-      throw new UnauthorizedException();
+      return null;
     }
+  }
+
+  private async getApiKeyUserId(token: string) {
+    const { id } = (await User.findOne({ where: { apiKey: token }, attributes: ["id"] })) ?? {};
+    return id;
   }
 }

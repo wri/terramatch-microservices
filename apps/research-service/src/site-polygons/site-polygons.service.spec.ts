451c05a620132f1baa312e8e9f2b9ec2a04f3dbb
Nathan Curtis <nathan.curtis@gmail.com>
2024-11-10 15:01:10 -0800

    [TM-1451] Unit test for getEstablishmentTreeSpecies

--- apps/research-service/src/site-polygons/site-polygons.service.spec.ts
+++ apps/research-service/src/site-polygons/site-polygons.service.spec.ts
@@ -9,7 +9,8 @@ import {
   IndicatorOutputTreeCoverLossFactory,
   SitePolygonFactory
 } from "@terramatch-microservices/database/factories";
-import { Indicator } from "@terramatch-microservices/database/entities";
+import { Indicator, TreeSpecies } from "@terramatch-microservices/database/entities";
+import { TreeSpeciesFactory } from "@terramatch-microservices/database/factories/tree-species.factory";
 
 describe("SitePolygonsService", () => {
   let service: SitePolygonsService;
@@ -45,6 +46,23 @@ describe("SitePolygonsService", () => {
       const dto = findDto(indicator);
       expect(dto).not.toBeNull();
       expect(indicator).toMatchObject(dto);
+    }
+  });
+
+  it("should return all establishment tree species", async () => {
+    const sitePolygon = await SitePolygonFactory.create();
+    const site = await sitePolygon.loadSite();
+    await TreeSpeciesFactory.forSite.createMany(3, { speciesableId: site.id });
+
+    const treeSpecies = await site.loadTreeSpecies();
+    const treeSpeciesDto = await service.getEstablishmentTreeSpecies(sitePolygon);
+    expect(treeSpeciesDto.length).toBe(treeSpecies.length);
+
+    const findDto = ({ name }: TreeSpecies) => treeSpeciesDto.find(dto => dto.name === name);
+    for (const tree of treeSpecies) {
+      const dto = findDto(tree);
+      expect(dto).not.toBeNull();
+      expect(tree).toMatchObject(dto);
     }
   });
 });

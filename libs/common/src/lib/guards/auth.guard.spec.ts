aa9a63edc283c293c4afeb01c46991695f0f5701
Nathan Curtis <nathan.curtis@gmail.com>
2024-11-10 21:15:26 -0800

    [TM-1451] Coverage for the common lib.

--- libs/common/src/lib/guards/auth.guard.spec.ts
+++ libs/common/src/lib/guards/auth.guard.spec.ts
@@ -1,37 +1,40 @@
-import { AuthGuard, NoBearerAuth } from './auth.guard';
-import { Test } from '@nestjs/testing';
-import { APP_GUARD } from '@nestjs/core';
-import { createMock, DeepMocked } from '@golevelup/ts-jest';
-import { JwtService } from '@nestjs/jwt';
-import { Controller, Get, HttpStatus, INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
+import { AuthGuard, NoBearerAuth } from "./auth.guard";
+import { Test } from "@nestjs/testing";
+import { APP_GUARD } from "@nestjs/core";
+import { createMock, DeepMocked } from "@golevelup/ts-jest";
+import { JwtService } from "@nestjs/jwt";
+import { Controller, Get, HttpStatus, INestApplication } from "@nestjs/common";
+import * as request from "supertest";
+import { UserFactory } from "@terramatch-microservices/database/factories";
 
-@Controller('test')
+@Controller("test")
 class TestController {
   @Get()
   test() {
-    return 'test';
+    return "test";
   }
 
   @NoBearerAuth
-  @Get('/no-auth')
+  @Get("/no-auth")
   noAuth() {
-    return 'no-auth';
+    return "no-auth";
   }
 }
 
-describe('AuthGuard', () => {
+describe("AuthGuard", () => {
   let jwtService: DeepMocked<JwtService>;
   let app: INestApplication;
 
   beforeEach(async () => {
-    app = (await Test.createTestingModule({
-      controllers: [TestController],
-      providers: [
-        { provide: JwtService, useValue: jwtService = createMock<JwtService>() },
-        { provide: APP_GUARD, useClass: AuthGuard },
-      ],
-    }).compile()).createNestApplication();
+    app = (
+      await Test.createTestingModule({
+        controllers: [TestController],
+        providers: [
+          { provide: JwtService, useValue: (jwtService = createMock<JwtService>()) },
+          { provide: APP_GUARD, useClass: AuthGuard }
+        ]
+      }).compile()
+    ).createNestApplication();
     await app.init();
   });
 
@@ -40,30 +43,40 @@ describe('AuthGuard', () => {
     jest.restoreAllMocks();
   });
 
-  it('should return an error when no auth header is present', async () => {
+  it("should return an error when no auth header is present", async () => {
+    await request(app.getHttpServer()).get("/test").expect(HttpStatus.UNAUTHORIZED);
+  });
+
+  it("should not return an error when a valid auth header is present", async () => {
+    const token = "fake jwt token";
+    jwtService.verifyAsync.mockResolvedValue({ sub: "fakeuserid" });
+
+    await request(app.getHttpServer()).get("/test").set("Authorization", `Bearer ${token}`).expect(HttpStatus.OK);
+  });
+
+  it("should ignore bearer token on an endpoint with @NoBearerAuth", async () => {
+    await request(app.getHttpServer()).get("/test/no-auth").expect(HttpStatus.OK);
+
     await request(app.getHttpServer())
-      .get('/test')
+      .get("/test/no-auth")
+      .set("Authorization", "Bearer fake jwt token")
+      .expect(HttpStatus.OK);
+  });
+
+  it("should use an api key for login", async () => {
+    const apiKey = "fake-api-key";
+    await UserFactory.create({ apiKey });
+    jwtService.decode.mockReturnValue(null);
+
+    await request(app.getHttpServer()).get("/test").set("Authorization", `Bearer ${apiKey}`).expect(HttpStatus.OK);
+  });
+
+  it("should throw when the api key is not recognized", async () => {
+    jwtService.decode.mockReturnValue(null);
+
+    await request(app.getHttpServer())
+      .get("/test")
+      .set("Authorization", "Bearer foobar")
       .expect(HttpStatus.UNAUTHORIZED);
-  });
-
-  it('should not return an error when a valid auth header is present', async () => {
-    const token = 'fake jwt token';
-    jwtService.verifyAsync.mockResolvedValue({ sub: 'fakeuserid' });
-
-    await request(app.getHttpServer())
-      .get('/test')
-      .set('Authorization', `Bearer ${token}`)
-      .expect(HttpStatus.OK);
-  });
-
-  it('should ignore bearer token on an endpoint with @NoBearerAuth', async () => {
-    await request(app.getHttpServer())
-      .get('/test/no-auth')
-      .expect(HttpStatus.OK);
-
-    await request(app.getHttpServer())
-      .get('/test/no-auth')
-      .set('Authorization', 'Bearer fake jwt token')
-      .expect(HttpStatus.OK);
   });
 });
---
+++ libs/common/src/lib/policies/site-polygon.policy.spec.ts
@@ -0,0 +1,33 @@
+import { PolicyService } from "./policy.service";
+import { Test, TestingModule } from "@nestjs/testing";
+import { mockPermissions, mockUserId } from "./policy.service.spec";
+import { SitePolygon } from "@terramatch-microservices/database/entities";
+import { UnauthorizedException } from "@nestjs/common";
+
+describe("SitePolygonPolicy", () => {
+  let service: PolicyService;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [PolicyService]
+    }).compile();
+
+    service = module.get<PolicyService>(PolicyService);
+  });
+
+  afterEach(async () => {
+    jest.restoreAllMocks();
+  });
+
+  it("allows reading any polygon with polygons-manage", async () => {
+    mockUserId(123);
+    mockPermissions("polygons-manage");
+    await expect(service.authorize("readAll", SitePolygon)).resolves.toBeUndefined();
+  });
+
+  it("disallows reading polygons without polygons-manage", async () => {
+    mockUserId(123);
+    mockPermissions();
+    await expect(service.authorize("readAll", SitePolygon)).rejects.toThrow(UnauthorizedException);
+  });
+});

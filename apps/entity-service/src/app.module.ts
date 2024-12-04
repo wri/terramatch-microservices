7d898ac06bb20a8d0c8b7372c08cf6242224611c
Nathan Curtis <nathan.curtis@gmail.com>
2024-12-04 15:39:31 -0800

    [TM-1401] Create the scientific names search endpoint.

--- apps/entity-service/src/app.module.ts
+++ apps/entity-service/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from "@nestjs/common";
 import { DatabaseModule } from "@terramatch-microservices/database";
 import { CommonModule } from "@terramatch-microservices/common";
 import { HealthModule } from "./health/health.module";
+import { TreesController } from "./trees/trees.controller";
+import { ResearchService } from "./trees/research.service";
 
 @Module({
   imports: [DatabaseModule, CommonModule, HealthModule],
-  controllers: [],
-  providers: []
+  controllers: [TreesController],
+  providers: [ResearchService]
 })
 export class AppModule {}
---
+++ apps/entity-service/src/trees/trees.controller.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, Controller, Get, Query } from "@nestjs/common";
+import { ResearchService } from "./research.service";
+import { buildJsonApi } from "@terramatch-microservices/common/util";
+import { ScientificNameDto } from "./dto/scientific-name.dto";
+import { ApiOperation } from "@nestjs/swagger";
+import { JsonApiResponse } from "@terramatch-microservices/common/decorators";
+
+@Controller("trees/v3")
+export class TreesController {
+  constructor(private readonly researchService: ResearchService) {}
+
+  @Get("scientific-names")
+  @ApiOperation({
+    operationId: "treeScientificNames",
+    description: "Search scientific names of tree species. Returns up to 10 entries"
+  })
+  @JsonApiResponse({ data: { type: ScientificNameDto } })
+  async searchScientificNames(@Query("search") search: string) {
+    if (search == null) throw new BadRequestException("search query param is required");
+
+    const document = buildJsonApi();
+    for (const treeSpecies of await this.researchService.searchScientificNames(search)) {
+      document.addData(`${treeSpecies.taxonId}`, new ScientificNameDto(treeSpecies));
+    }
+
+    return document.serialize();
+  }
+}

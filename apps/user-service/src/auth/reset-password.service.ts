746b75093095e043dcab152ccf33855710e6cd12
Ismael Martinez <admin@scriptmatico.com>
2025-02-04 14:09:12 -0600

    comments corrections - remove unused import

--- apps/user-service/src/auth/reset-password.service.ts
+++ apps/user-service/src/auth/reset-password.service.ts
@@ -1,14 +1,13 @@
-import { Injectable, NotFoundException, BadRequestException, Logger, LoggerService } from "@nestjs/common";
-import bcrypt from 'bcryptjs';
-import { JwtService } from '@nestjs/jwt';
-import { User } from '@terramatch-microservices/database/entities';
+import { Injectable, NotFoundException, BadRequestException, LoggerService } from "@nestjs/common";
+import bcrypt from "bcryptjs";
+import { JwtService } from "@nestjs/jwt";
+import { User } from "@terramatch-microservices/database/entities";
 import { EmailService } from "@terramatch-microservices/common/email/email.service";
 import { LocalizationService } from "@terramatch-microservices/common/localization/localization.service";
 import { TMLogService } from "@terramatch-microservices/common/util/tm-log.service";
 
 @Injectable()
 export class ResetPasswordService {
-
   protected readonly logger: LoggerService = new TMLogService(ResetPasswordService.name);
 
   constructor(
@@ -20,27 +19,27 @@ export class ResetPasswordService {
   async sendResetPasswordEmail(emailAddress: string, callbackUrl: string) {
     const user = await User.findOne({ where: { emailAddress }, attributes: ["id", "uuid", "locale"] });
     if (user == null) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException("User not found");
     }
 
-    const bodyKey = 'reset-password.body';
-    const subjectKey = 'reset-password.subject';
+    const bodyKey = "reset-password.body";
+    const subjectKey = "reset-password.subject";
 
     const localizationKeys = await this.localizationService.getLocalizationKeys([bodyKey, subjectKey]);
 
     if (!localizationKeys.length) {
-      throw new NotFoundException('Localizations not found');
+      throw new NotFoundException("Localizations not found");
     }
 
     const bodyLocalization = localizationKeys.find(x => x.key == bodyKey);
     const subjectLocalization = localizationKeys.find(x => x.key == subjectKey);
 
     if (bodyLocalization == null) {
-      throw new NotFoundException('Localization body not found');
+      throw new NotFoundException("Localization body not found");
     }
 
     if (subjectLocalization == null) {
-      throw new NotFoundException('Localization subject not found');
+      throw new NotFoundException("Localization subject not found");
     }
 
     const resetToken = await this.jwtService.signAsync({ sub: user.uuid }, { expiresIn: "2h" });
@@ -55,7 +54,7 @@ export class ResetPasswordService {
 
   private formatBody(bodyEmailContent: string, resetLink: string) {
     const anchor = `<a href="${resetLink}" target="_blank">link</a>`;
-    return bodyEmailContent.replace('link', anchor).replace('enlace', anchor).replace('lien', anchor);
+    return bodyEmailContent.replace("link", anchor).replace("enlace", anchor).replace("lien", anchor);
   }
 
   async resetPassword(resetToken: string, newPassword: string) {
@@ -65,12 +64,12 @@ export class ResetPasswordService {
       userGuid = payload.sub;
     } catch (error) {
       this.logger.error(error);
-      throw new BadRequestException('Provided token is invalid or expired');
+      throw new BadRequestException("Provided token is invalid or expired");
     }
 
     const user = await User.findOne({ where: { uuid: userGuid }, attributes: ["id", "uuid"] });
     if (!user) {
-      throw new NotFoundException('User not found');
+      throw new NotFoundException("User not found");
     }
 
     const hashedPassword = await bcrypt.hash(newPassword, 10);

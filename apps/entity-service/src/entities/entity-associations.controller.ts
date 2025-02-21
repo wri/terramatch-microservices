3bebb4851b29dff502e2bf4e84fec61cb916113b
Nathan Curtis <nathan.curtis@gmail.com>
2025-02-21 15:38:57 -0800

    [TM-1718] Get new endpoint documentation in place.

--- apps/entity-service/src/entities/entity-associations.controller.ts
+++ apps/entity-service/src/entities/entity-associations.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, Param } from "@nestjs/common";
+import { BadRequestException, Controller, Get, NotFoundException, Param } from "@nestjs/common";
 import { EntityAssociationIndexParamsDto } from "./dto/entity-association-index-params.dto";
 import { EntitiesService } from "./entities.service";
 import { PolicyService } from "@terramatch-microservices/common";
 import { buildJsonApi } from "@terramatch-microservices/common/util";
-import { ApiExtraModels } from "@nestjs/swagger";
-import { DemographicEntryDto } from "./dto/demographic.dto";
+import { ApiExtraModels, ApiOperation } from "@nestjs/swagger";
+import { DemographicDto, DemographicEntryDto } from "./dto/demographic.dto";
+import { ExceptionResponse, JsonApiResponse } from "@terramatch-microservices/common/decorators";
 
 @Controller("entities/v3/:entity/:uuid")
 @ApiExtraModels(DemographicEntryDto)
@@ -12,6 +13,13 @@ export class EntityAssociationsController {
   constructor(private readonly entitiesService: EntitiesService, private readonly policyService: PolicyService) {}
 
   @Get(":association")
+  @ApiOperation({
+    operationId: "entityAssociationIndex",
+    summary: "Get all of a single type of association that are related to a given entity."
+  })
+  @JsonApiResponse([DemographicDto])
+  @ExceptionResponse(BadRequestException, { description: "Param types invalid" })
+  @ExceptionResponse(NotFoundException, { description: "Base entity not found" })
   async associationIndex(@Param() { entity, uuid, association }: EntityAssociationIndexParamsDto) {
     const processor = this.entitiesService.createAssociationProcessor(entity, uuid, association);
     const baseEntity = await processor.getBaseEntity();

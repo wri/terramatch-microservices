356a0258e6dd25293430520fcf544a6af77edc06
Nathan Curtis <nathan.curtis@gmail.com>
2025-02-25 16:00:52 -0800

    [TM-1718] Coverage for new policies.

---
+++ libs/common/src/lib/policies/project-report.policy.spec.ts
@@ -0,0 +1,82 @@
+import { PolicyService } from "./policy.service";
+import { Test } from "@nestjs/testing";
+import { expectCan, expectCannot, mockPermissions, mockUserId } from "./policy.service.spec";
+import { ProjectReport } from "@terramatch-microservices/database/entities";
+import {
+  OrganisationFactory,
+  ProjectFactory,
+  ProjectReportFactory,
+  ProjectUserFactory,
+  UserFactory
+} from "@terramatch-microservices/database/factories";
+
+describe("ProjectReportPolicy", () => {
+  let service: PolicyService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [PolicyService]
+    }).compile();
+
+    service = await module.resolve(PolicyService);
+  });
+
+  afterEach(async () => {
+    jest.restoreAllMocks();
+  });
+
+  it("allows reading all project reports with view-dashboard permissions", async () => {
+    mockUserId(123);
+    mockPermissions("view-dashboard");
+    await expectCan(service, "read", new ProjectReport());
+  });
+
+  it("allows reading project reports in your framework", async () => {
+    mockUserId(123);
+    mockPermissions("framework-ppc");
+    const ppc = await ProjectReportFactory.create({ frameworkKey: "ppc" });
+    const tf = await ProjectReportFactory.create({ frameworkKey: "terrafund" });
+    await expectCan(service, "read", ppc);
+    await expectCannot(service, "read", tf);
+  });
+
+  it("allows reading project reports for own projects", async () => {
+    mockPermissions("manage-own");
+    const org = await OrganisationFactory.create();
+    const user = await UserFactory.create({ organisationId: org.id });
+    mockUserId(user.id);
+
+    const p1 = await ProjectFactory.create({ organisationId: org.id });
+    const p2 = await ProjectFactory.create();
+    const p3 = await ProjectFactory.create();
+    const p4 = await ProjectFactory.create();
+    await ProjectUserFactory.create({ userId: user.id, projectId: p3.id });
+    await ProjectUserFactory.create({ userId: user.id, projectId: p4.id, isMonitoring: false, isManaging: true });
+
+    const pr1 = await ProjectReportFactory.create({ projectId: p1.id });
+    const pr2 = await ProjectReportFactory.create({ projectId: p2.id });
+    const pr3 = await ProjectReportFactory.create({ projectId: p3.id });
+    const pr4 = await ProjectReportFactory.create({ projectId: p4.id });
+
+    await expectCan(service, "read", pr1);
+    await expectCannot(service, "read", pr2);
+    await expectCan(service, "read", pr3);
+    await expectCan(service, "read", pr4);
+  });
+
+  it("allows reading project reports for managed projects", async () => {
+    mockPermissions("projects-manage");
+    const user = await UserFactory.create();
+    mockUserId(user.id);
+
+    const p1 = await ProjectFactory.create();
+    const p2 = await ProjectFactory.create();
+    await ProjectUserFactory.create({ userId: user.id, projectId: p1.id, isMonitoring: false, isManaging: true });
+
+    const pr1 = await ProjectReportFactory.create({ projectId: p1.id });
+    const pr2 = await ProjectReportFactory.create({ projectId: p2.id });
+
+    await expectCan(service, "read", pr1);
+    await expectCannot(service, "read", pr2);
+  });
+});
---
+++ libs/common/src/lib/policies/site-report.policy.spec.ts
@@ -0,0 +1,91 @@
+import { PolicyService } from "./policy.service";
+import { Test } from "@nestjs/testing";
+import { expectCan, expectCannot, mockPermissions, mockUserId } from "./policy.service.spec";
+import { SiteReport } from "@terramatch-microservices/database/entities";
+import {
+  OrganisationFactory,
+  ProjectFactory,
+  ProjectUserFactory,
+  SiteFactory,
+  SiteReportFactory,
+  UserFactory
+} from "@terramatch-microservices/database/factories";
+
+describe("SiteReportPolicy", () => {
+  let service: PolicyService;
+
+  beforeEach(async () => {
+    const module = await Test.createTestingModule({
+      providers: [PolicyService]
+    }).compile();
+
+    service = await module.resolve(PolicyService);
+  });
+
+  afterEach(async () => {
+    jest.restoreAllMocks();
+  });
+
+  it("allows reading all site reports with view-dashboard permissions", async () => {
+    mockUserId(123);
+    mockPermissions("view-dashboard");
+    await expectCan(service, "read", new SiteReport());
+  });
+
+  it("allows reading site reports in your framework", async () => {
+    mockUserId(123);
+    mockPermissions("framework-ppc");
+    const ppc = await SiteReportFactory.create({ frameworkKey: "ppc" });
+    const tf = await SiteReportFactory.create({ frameworkKey: "terrafund" });
+    await expectCan(service, "read", ppc);
+    await expectCannot(service, "read", tf);
+  });
+
+  it("allows reading site reports for own projects", async () => {
+    mockPermissions("manage-own");
+    const org = await OrganisationFactory.create();
+    const user = await UserFactory.create({ organisationId: org.id });
+    mockUserId(user.id);
+
+    const p1 = await ProjectFactory.create({ organisationId: org.id });
+    const p2 = await ProjectFactory.create();
+    const p3 = await ProjectFactory.create();
+    const p4 = await ProjectFactory.create();
+    await ProjectUserFactory.create({ userId: user.id, projectId: p3.id });
+    await ProjectUserFactory.create({ userId: user.id, projectId: p4.id, isMonitoring: false, isManaging: true });
+
+    const s1 = await SiteFactory.create({ projectId: p1.id });
+    const s2 = await SiteFactory.create({ projectId: p2.id });
+    const s3 = await SiteFactory.create({ projectId: p3.id });
+    const s4 = await SiteFactory.create({ projectId: p4.id });
+
+    const sr1 = await SiteReportFactory.create({ siteId: s1.id });
+    const sr2 = await SiteReportFactory.create({ siteId: s2.id });
+    const sr3 = await SiteReportFactory.create({ siteId: s3.id });
+    const sr4 = await SiteReportFactory.create({ siteId: s4.id });
+
+    await expectCan(service, "read", sr1);
+    await expectCannot(service, "read", sr2);
+    await expectCan(service, "read", sr3);
+    await expectCan(service, "read", sr4);
+  });
+
+  it("allows reading site reports for managed projects", async () => {
+    mockPermissions("projects-manage");
+    const user = await UserFactory.create();
+    mockUserId(user.id);
+
+    const p1 = await ProjectFactory.create();
+    const p2 = await ProjectFactory.create();
+    await ProjectUserFactory.create({ userId: user.id, projectId: p1.id, isMonitoring: false, isManaging: true });
+
+    const s1 = await SiteFactory.create({ projectId: p1.id });
+    const s2 = await SiteFactory.create({ projectId: p2.id });
+
+    const sr1 = await SiteReportFactory.create({ siteId: s1.id });
+    const sr2 = await SiteReportFactory.create({ siteId: s2.id });
+
+    await expectCan(service, "read", sr1);
+    await expectCannot(service, "read", sr2);
+  });
+});

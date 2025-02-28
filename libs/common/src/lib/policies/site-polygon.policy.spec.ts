95ef458d37e3fac258e8d215b2c42215e338ce94
JORGE <egrojmonroy@gmail.com>
2025-02-28 17:43:25 -0400

    [TM-1759] adding site polygon tests

--- libs/common/src/lib/policies/site-polygon.policy.spec.ts
+++ libs/common/src/lib/policies/site-polygon.policy.spec.ts
@@ -2,6 +2,7 @@ import { PolicyService } from "./policy.service";
 import { Test, TestingModule } from "@nestjs/testing";
 import { expectCan, expectCannot, mockPermissions, mockUserId } from "./policy.service.spec";
 import { SitePolygon } from "@terramatch-microservices/database/entities";
+import { SiteFactory } from "@terramatch-microservices/database/factories";
 
 describe("SitePolygonPolicy", () => {
   let service: PolicyService;
@@ -12,21 +13,34 @@ describe("SitePolygonPolicy", () => {
     }).compile();
 
     service = await module.resolve<PolicyService>(PolicyService);
-
-    mockUserId(123);
   });
 
   afterEach(async () => {
     jest.restoreAllMocks();
   });
 
-  it("allows reading any polygon with polygons-manage", async () => {
-    mockPermissions("polygons-manage");
+  it("allows reading any polygon with polygons-manage and frameworks", async () => {
+    const site = await SiteFactory.create({ frameworkKey: "ppc" });
+
+    mockUserId(123);
+    mockPermissions("polygons-manage", "framework-ppc");
+    const sitePolygon = new SitePolygon();
+    sitePolygon.siteUuid = site.uuid;
+
     await expectCan(service, "readAll", SitePolygon);
+    await expectCan(service, "manage", sitePolygon);
   });
 
   it("disallows reading polygons without polygons-manage", async () => {
+    mockUserId(123);
     mockPermissions();
     await expectCannot(service, "readAll", SitePolygon);
+  });
+
+  it("disallows managing polygons with polygons-manage but no frameworks", async () => {
+    mockUserId(123);
+    mockPermissions("polygons-manage");
+    const sitePolygon = new SitePolygon();
+    await expectCannot(service, "manage", sitePolygon);
   });
 });

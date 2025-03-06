04ba5ffeb224aad4392b0b9268a2922bf21e1d18
Limber Mamani <limbermv2@gmail.com>
2025-03-06 14:10:51 -0400

    [TM-1767] add filters to query

--- apps/entity-service/src/entities/processors/nursery.processor.ts
+++ apps/entity-service/src/entities/processors/nursery.processor.ts
@@ -1,9 +1,11 @@
-import { Media, Nursery, NurseryReport, Seeding } from "@terramatch-microservices/database/entities";
+import { Media, Nursery, NurseryReport, ProjectUser } from "@terramatch-microservices/database/entities";
 import { NurseryLightDto, NurseryFullDto, AdditionalNurseryFullProps, NurseryMedia } from "../dto/nursery.dto";
 import { EntityProcessor, PaginatedResult } from "./entity-processor";
 import { EntityQueryDto } from "../dto/entity-query.dto";
 import { DocumentBuilder } from "@terramatch-microservices/common/util";
 import { Includeable, Op } from "sequelize";
+import { BadRequestException } from "@nestjs/common";
+import { FrameworkKey } from "@terramatch-microservices/database/constants/framework";
 
 export class NurseryProcessor extends EntityProcessor<Nursery, NurseryLightDto, NurseryFullDto> {
   readonly LIGHT_DTO = NurseryLightDto;
@@ -22,7 +24,7 @@ export class NurseryProcessor extends EntityProcessor<Nursery, NurseryLightDto, 
     });
   }
 
-  async findMany(query: EntityQueryDto): Promise<PaginatedResult<Nursery>> {
+  async findMany(query: EntityQueryDto, userId?: number, permissions?: string[]): Promise<PaginatedResult<Nursery>> {
     const projectAssociation: Includeable = {
       association: "project",
       attributes: ["uuid", "name"],
@@ -31,6 +33,46 @@ export class NurseryProcessor extends EntityProcessor<Nursery, NurseryLightDto, 
 
     const associations = [projectAssociation];
     const builder = await this.entitiesService.buildQuery(Nursery, query, associations);
+    if (query.sort != null) {
+      if (["name", "startDate"].includes(query.sort.field)) {
+        builder.order([query.sort.field, query.sort.direction ?? "ASC"]);
+      } else if (query.sort.field === "organisationName") {
+        builder.order(["project", "organisation", "name", query.sort.direction ?? "ASC"]);
+      } else if (query.sort.field === "projectName") {
+        builder.order(["project", "name", query.sort.direction ?? "ASC"]);
+      } else if (query.sort.field !== "id") {
+        throw new BadRequestException(`Invalid sort field: ${query.sort.field}`);
+      }
+    }
+
+    const frameworkPermissions = permissions
+      .filter(name => name.startsWith("framework-"))
+      .map(name => name.substring("framework-".length) as FrameworkKey);
+    if (frameworkPermissions.length > 0) {
+      builder.where({ frameworkKey: { [Op.in]: frameworkPermissions } });
+    } else if (permissions.includes("manage-own")) {
+      builder.where({ projectId: { [Op.in]: ProjectUser.userProjectsSubquery(userId) } });
+    } else if (permissions.includes("projects-manage")) {
+      builder.where({ projectId: { [Op.in]: ProjectUser.projectsManageSubquery(userId) } });
+    }
+
+    for (const term of ["status", "updateRequestStatus", "frameworkKey"]) {
+      if (query[term] != null) builder.where({ [term]: query[term] });
+    }
+
+    if (query.search != null) {
+      builder.where({ name: { [Op.like]: `%${query.search}%` } });
+    }
+
+    //filter to projectUuid
+    // if (query.projectUuid != null) {
+    //   const project = await Project.findOne({ where: { uuid: query.projectUuid }, attributes: ["id"] });
+    //   if (project == null) {
+    //     throw new BadRequestException(`Project with uuid ${query.projectUuid} not found`);
+    //   }
+    //   builder.where({ projectId: project.id });
+    // }
+
     return { models: await builder.execute(), paginationTotal: await builder.paginationTotal() };
   }
 

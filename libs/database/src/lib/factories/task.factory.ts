8c507cc8cd812af8698994af936c52e118b94e94
LimberHope <limbermv2@gmail.com>
2025-03-21 12:02:26 -0400

    [TM-1768] fix test

--- libs/database/src/lib/factories/task.factory.ts
+++ libs/database/src/lib/factories/task.factory.ts
@@ -1,6 +1,5 @@
 import { FactoryGirl } from "factory-girl-ts";
 import { Task } from "../entities";
-import { ProjectReportFactory } from "./project-report.factory";
 
 export const TaskFactory = FactoryGirl.define(Task, async () => {
   return {

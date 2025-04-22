7efda86e683311f09edda66a0624659203ca8df9
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-04-22 09:28:12 -0400

    [TM-1772] fix typing

--- apps/entity-service/src/entities/dto/media-association.dto.ts
+++ apps/entity-service/src/entities/dto/media-association.dto.ts
@@ -2,10 +2,10 @@ import { Media } from "@terramatch-microservices/database/entities";
 import { AssociationDto, AssociationDtoAdditionalProps } from "./association.dto";
 import { UserDto } from "@terramatch-microservices/common/dto/user.dto";
 import { pickApiProperties } from "@terramatch-microservices/common/dto/json-api-attributes";
-export interface MediaAssociationDtoAdditionalProps extends AssociationDtoAdditionalProps {
+export type MediaAssociationDtoAdditionalProps = AssociationDtoAdditionalProps & {
   url: string;
   thumbUrl: string;
-}
+};
 
 export class MediaAssociationDto extends AssociationDto<MediaAssociationDto> {
   constructor(media: Media, additional: MediaAssociationDtoAdditionalProps) {

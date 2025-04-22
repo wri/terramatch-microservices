c4572ff95ab9732dd174e6ea6ea7adcb872895af
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-04-22 11:11:22 -0400

    [TM-1772] add newline

--- apps/entity-service/src/entities/dto/media-association.dto.ts
+++ apps/entity-service/src/entities/dto/media-association.dto.ts
@@ -1,7 +1,7 @@
 import { Media } from "@terramatch-microservices/database/entities";
 import { AssociationDto, AssociationDtoAdditionalProps } from "./association.dto";
-import { UserDto } from "@terramatch-microservices/common/dto/user.dto";
 import { pickApiProperties } from "@terramatch-microservices/common/dto/json-api-attributes";
+
 export type MediaAssociationDtoAdditionalProps = AssociationDtoAdditionalProps & {
   url: string;
   thumbUrl: string;

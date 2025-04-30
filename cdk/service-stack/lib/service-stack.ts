879d2e6ff16bc19d7c56e2aba87bc767712ff972
Nathan Curtis <nathan.curtis@gmail.com>
2025-04-30 16:58:06 -0700

    [TM-1931] Modify configurations for cpu / memory based on AWS recommendations.

--- cdk/service-stack/lib/service-stack.ts
+++ cdk/service-stack/lib/service-stack.ts
@@ -10,6 +10,7 @@ import {
 } from "aws-cdk-lib/aws-ecs-patterns";
 import { Role } from "aws-cdk-lib/aws-iam";
 import { upperFirst } from "lodash";
+import { Dictionary } from "factory-girl-ts";
 
 const extractFromEnv = (...names: string[]) =>
   names.map(name => {
@@ -22,11 +23,20 @@ type Mutable<T> = {
   -readonly [P in keyof T]: T[P];
 };
 
+// Recommendations for optimal pricing from AWs
+const RIGHTSIZE_RECOMMENDATIONS: Dictionary<Dictionary<{ cpu: number; memoryLimitMiB: number }>> = {
+  "research-service": {
+    prod: {
+      cpu: 1024,
+      memoryLimitMiB: 2048
+    }
+  }
+};
+
 const customizeFargate = (service: string, env: string, props: Mutable<ApplicationLoadBalancedFargateServiceProps>) => {
-  if (service === "research-service" && ["prod", "staging"].includes(env)) {
-    // Beef up the research service in staging and prod
-    props.cpu = 2048;
-    props.memoryLimitMiB = 4096;
+  const recommendation = RIGHTSIZE_RECOMMENDATIONS[service]?.[env];
+  if (recommendation != null) {
+    return { ...props, ...recommendation };
   }
 
   return props;
@@ -82,7 +92,9 @@ export class ServiceStack extends Stack {
       customizeFargate(service, env, {
         serviceName: `terramatch-${service}-${env}`,
         cluster,
-        cpu: 512,
+        // These are the recommended defaults by Amazon Rightsize in the billing console
+        cpu: 256,
+        memoryLimitMiB: 512,
         desiredCount: 1,
         enableExecuteCommand: true,
         taskImageOptions: {
@@ -97,7 +109,6 @@ export class ServiceStack extends Stack {
         },
         securityGroups: securityGroups,
         taskSubnets: { subnets: privateSubnets },
-        memoryLimitMiB: 2048,
         assignPublicIp: false,
         publicLoadBalancer: false,
         loadBalancerName: `${service}-${env}`

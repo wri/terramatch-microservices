1aa85d664f64bb7f7d3fedcba29879bc33597fbb
LimberHope <limbermv2@gmail.com>
2025-05-07 15:29:49 -0400

    [TM-2008] add dashboard query dto

---
+++ apps/dashboard-service/src/dashboard/dto/dashboard-query.dto.ts
@@ -0,0 +1,52 @@
+import { ApiProperty, IntersectionType } from "@nestjs/swagger";
+import { IsEnum, IsOptional, ValidateNested } from "class-validator";
+import { NumberPage } from "@terramatch-microservices/common/dto/page.dto";
+
+class QuerySort {
+  @ApiProperty({ name: "sort[field]", required: false })
+  @IsOptional()
+  field?: string;
+
+  @ApiProperty({ name: "sort[direction]", required: false, enum: ["ASC", "DESC"], default: "ASC" })
+  @IsEnum(["ASC", "DESC"])
+  @IsOptional()
+  direction?: "ASC" | "DESC";
+}
+
+export class DashboardQueryDto extends IntersectionType(QuerySort, NumberPage) {
+  @ValidateNested()
+  @IsOptional()
+  page?: NumberPage;
+
+  @ValidateNested()
+  @IsOptional()
+  sort?: QuerySort;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  search?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  country?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  programmes?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  cohort?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  landscapes?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  organisationType?: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  projectUuid?: string;
+}

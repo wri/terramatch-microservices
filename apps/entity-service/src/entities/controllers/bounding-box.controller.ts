ab4406ec2c5497f5180fdd05ec2e11cfa894e20e
JORGE <egrojmonroy@gmail.com>
2025-05-19 16:33:57 -0400

    [TM-2040] fix generation

--- apps/entity-service/src/entities/controllers/bounding-box.controller.ts
+++ apps/entity-service/src/entities/controllers/bounding-box.controller.ts
@@ -1,9 +1,10 @@
 import { Controller, Get, Query } from "@nestjs/common";
-import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import { ApiTags, ApiOperation } from "@nestjs/swagger";
 import { BoundingBoxService } from "../services/bounding-box.service";
 import { BoundingBoxQueryDto } from "../dto/bounding-box-query.dto";
 import { BoundingBoxDto } from "../dto/bounding-box.dto";
 import { JsonApiResponse } from "@terramatch-microservices/common/decorators";
+import { buildJsonApi, JsonApiDocument } from "@terramatch-microservices/common/util";
 
 @Controller("v3/boundingBoxes")
 @ApiTags("Bounding Boxes")
@@ -11,23 +12,34 @@ export class BoundingBoxController {
   constructor(private readonly boundingBoxService: BoundingBoxService) {}
 
   @Get()
-  @ApiOkResponse({ description: "Bounding box retrieved successfully" })
-  @JsonApiResponse({ data: BoundingBoxDto })
-  async getBoundingBox(@Query() query: BoundingBoxQueryDto): Promise<BoundingBoxDto> {
+  @ApiOperation({
+    operationId: "boundingBoxGet",
+    summary: "Get a bounding box for a polygon, site, project, or country/landscape"
+  })
+  @JsonApiResponse(BoundingBoxDto)
+  async getBoundingBox(@Query() query: BoundingBoxQueryDto): Promise<JsonApiDocument> {
+    let result: BoundingBoxDto;
+
     if (query.polygonUuid) {
-      return this.boundingBoxService.getPolygonBoundingBox(query.polygonUuid);
+      result = await this.boundingBoxService.getPolygonBoundingBox(query.polygonUuid);
+      return buildJsonApi(BoundingBoxDto).addData(query.polygonUuid, result).document.serialize();
     }
 
     if (query.siteUuid) {
-      return this.boundingBoxService.getSiteBoundingBox(query.siteUuid);
+      result = await this.boundingBoxService.getSiteBoundingBox(query.siteUuid);
+      return buildJsonApi(BoundingBoxDto).addData(query.siteUuid, result).document.serialize();
     }
 
     if (query.projectUuid) {
-      return this.boundingBoxService.getProjectBoundingBox(query.projectUuid);
+      result = await this.boundingBoxService.getProjectBoundingBox(query.projectUuid);
+      return buildJsonApi(BoundingBoxDto).addData(query.projectUuid, result).document.serialize();
     }
 
     if (query.country || (query.landscapes && query.landscapes.length)) {
-      return this.boundingBoxService.getCountryLandscapeBoundingBox(query.country, query.landscapes || []);
+      result = await this.boundingBoxService.getCountryLandscapeBoundingBox(query.country, query.landscapes || []);
+      // Generate a stable ID for this bounding box
+      const id = `${query.country || "global"}-${(query.landscapes || []).join("-")}`;
+      return buildJsonApi(BoundingBoxDto).addData(id, result).document.serialize();
     }
 
     throw new Error(

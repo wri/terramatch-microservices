b79d72412ce21204dd51ed164df442d1c593760f
JORGE <egrojmonroy@gmail.com>
2025-05-22 17:27:51 -0400

    [TM-2040] update site polygon policy for manage-own users

--- libs/common/src/lib/policies/site-polygon.policy.ts
+++ libs/common/src/lib/policies/site-polygon.policy.ts
@@ -1,4 +1,4 @@
-import { SitePolygon, Site } from "@terramatch-microservices/database/entities";
+import { SitePolygon, Site, User, Project } from "@terramatch-microservices/database/entities";
 import { UserPermissionsPolicy } from "./user-permissions.policy";
 import { Op } from "sequelize";
 
@@ -17,5 +17,41 @@ export class SitePolygonPolicy extends UserPermissionsPolicy {
       const siteUuids = sites.map(site => site.uuid);
       this.builder.can("manage", SitePolygon, { siteUuid: { $in: siteUuids } });
     }
+
+    if (this.permissions.includes("manage-own")) {
+      const user = await this.getUser();
+      if (user != null) {
+        const projectIds = [
+          ...(user.organisationId === null
+            ? []
+            : await Project.findAll({ where: { organisationId: user.organisationId }, attributes: ["id"] })
+          ).map(({ id }) => id),
+          ...user.projects.map(({ id }) => id)
+        ];
+
+        if (projectIds.length > 0) {
+          const sites = await Site.findAll({
+            where: { projectId: { [Op.in]: projectIds } },
+            attributes: ["uuid"]
+          });
+
+          const siteUuids = sites.map(site => site.uuid);
+          if (siteUuids.length > 0) {
+            this.builder.can("manage", SitePolygon, { siteUuid: { $in: siteUuids } });
+          }
+        }
+      }
+    }
+  }
+
+  protected _user?: User | null;
+  protected async getUser() {
+    if (this._user != null) return this._user;
+
+    return (this._user = await User.findOne({
+      where: { id: this.userId },
+      attributes: ["organisationId"],
+      include: [{ association: "projects", attributes: ["id"] }]
+    }));
   }
 }

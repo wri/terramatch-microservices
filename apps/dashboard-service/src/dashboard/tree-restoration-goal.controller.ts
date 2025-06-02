300fd913e33ed2835b6b05d7b0369901548a3003
JORGE <egrojmonroy@gmail.com>
2025-06-02 10:55:16 -0400

    [TM-2083] [TM-2040] cache results for improving timing

--- apps/dashboard-service/src/dashboard/tree-restoration-goal.controller.ts
+++ apps/dashboard-service/src/dashboard/tree-restoration-goal.controller.ts
@@ -4,16 +4,44 @@ import { DashboardQueryDto } from "./dto/dashboard-query.dto";
 import { JsonApiResponse } from "@terramatch-microservices/common/decorators";
 import { TreeRestorationGoalDto } from "./dto/tree-restoration-goal.dto";
 import { TreeRestorationGoalService } from "./dto/tree-restoration-goal.service";
+import { CacheService } from "./dto/cache.service";
+import { buildJsonApi, getStableRequestQuery } from "@terramatch-microservices/common/util/json-api-builder";
 
 @Controller("dashboard/v3/treeRestorationGoal")
 export class TreeRestorationGoalController {
-  constructor(private readonly treeRestorationGoalService: TreeRestorationGoalService) {}
+  constructor(
+    private readonly treeRestorationGoalService: TreeRestorationGoalService,
+    private readonly cacheService: CacheService
+  ) {}
 
   @Get()
   @JsonApiResponse([TreeRestorationGoalDto])
   @ApiOperation({ operationId: "getTreeRestorationGoal", summary: "Get tree restoration goal statistics" })
   async getTreeRestorationGoal(@Query() query: DashboardQueryDto) {
-    const result = await this.treeRestorationGoalService.getTreeRestorationGoal(query);
-    return result;
+    const cacheKey = `dashboard:tree-restoration-goal|${this.cacheService.getCacheKeyFromQuery(query)}`;
+    const timestampKey = `${cacheKey}:timestamp`;
+    const lastUpdatedAt = await this.cacheService.get(timestampKey);
+    const cachedData = await this.cacheService.get(cacheKey);
+
+    if (cachedData == null) {
+      const result = await this.treeRestorationGoalService.getTreeRestorationGoal(query);
+      const timestamp = new Date().toISOString();
+      await this.cacheService.set(cacheKey, JSON.stringify(result));
+      await this.cacheService.set(timestampKey, timestamp);
+      return result;
+    }
+
+    const document = buildJsonApi(TreeRestorationGoalDto);
+    const stableQuery = getStableRequestQuery(query);
+
+    document.addData(
+      stableQuery,
+      new TreeRestorationGoalDto({
+        ...cachedData,
+        lastUpdatedAt
+      })
+    );
+
+    return document.serialize();
   }
 }

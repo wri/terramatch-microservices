c2093ef35a3216a38f839c55a650f240f78991a6
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-06-09 10:23:45 -0400

    [TM-1775] remove unnecesary typing

--- instrument-sentry.ts
+++ instrument-sentry.ts
@@ -1,12 +1,11 @@
 import * as Sentry from "@sentry/nestjs";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
-import { Integration } from "@sentry/core";
 
 if (process.env.SENTRY_DSN != null) {
   Sentry.init({
     dsn: process.env.SENTRY_DSN,
     environment: process.env.DEPLOY_ENV,
-    integrations: [nodeProfilingIntegration() as Integration],
+    integrations: [nodeProfilingIntegration()],
     tracesSampleRate: 1.0,
     profilesSampleRate: 1.0
   });

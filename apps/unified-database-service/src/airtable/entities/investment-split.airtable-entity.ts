0b120e34839490faa1a27e1f486956f19e99fd7f
Nathan Curtis <nathan.curtis@gmail.com>
2025-07-03 11:47:16 -0700

    [TM-2202] Add investments and investment splits to Airtable.

---
+++ apps/unified-database-service/src/airtable/entities/investment-split.airtable-entity.ts
@@ -0,0 +1,19 @@
+import { AirtableEntity, ColumnMapping, commonEntityColumns } from "./airtable-entity";
+import { Investment, InvestmentSplit } from "@terramatch-microservices/database/entities";
+
+const COLUMNS: ColumnMapping<InvestmentSplit>[] = [
+  ...commonEntityColumns<InvestmentSplit>(),
+  {
+    airtableColumn: "investmentUuid",
+    include: [{ model: Investment, attributes: ["uuid"] }],
+    valueMap: async ({ investment }) => investment?.uuid
+  },
+  "funder",
+  "amount"
+];
+
+export class InvestmentSplitEntity extends AirtableEntity<InvestmentSplit> {
+  readonly TABLE_NAME = "Investment Splits";
+  readonly COLUMNS = COLUMNS;
+  readonly MODEL = InvestmentSplit;
+}
---
+++ apps/unified-database-service/src/airtable/entities/investment.airtable-entity.ts
@@ -0,0 +1,19 @@
+import { AirtableEntity, ColumnMapping, commonEntityColumns } from "./airtable-entity";
+import { Investment, Project } from "@terramatch-microservices/database/entities";
+
+const COLUMNS: ColumnMapping<Investment>[] = [
+  ...commonEntityColumns<Investment>(),
+  {
+    airtableColumn: "projectUuid",
+    include: [{ model: Project, attributes: ["uuid"] }],
+    valueMap: async ({ project }) => project?.uuid
+  },
+  "investmentDate",
+  "type"
+];
+
+export class InvestmentEntity extends AirtableEntity<Investment> {
+  readonly TABLE_NAME = "Investments";
+  readonly COLUMNS = COLUMNS;
+  readonly MODEL = Investment;
+}

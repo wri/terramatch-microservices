e0a30357fd2a3bf995d521803080f3082c73915f
Nathan Curtis <nathan.curtis@gmail.com>
2025-07-17 08:20:17 -0700

    [TM-2006] Consolidate framework consts / types.

--- libs/database/src/lib/constants/framework.ts
+++ libs/database/src/lib/constants/framework.ts
@@ -1,10 +1,13 @@
-export const FRAMEWORK_KEYS = [
-  "terrafund",
-  "terrafund-landscapes",
-  "ppc",
-  "enterprises",
-  "hbf",
-  "epa-ghana-pilot",
-  "fundo-flora"
-] as const;
+const TERRAFUND = "terrafund" as const;
+const LANDSCAPES = "terrafund-landscapes" as const;
+const PPC = "ppc" as const;
+const ENTERPRISES = "enterprises" as const;
+const HBF = "hbf" as const;
+const EPA = "epa-ghana-pilot" as const;
+const FUNDO_FLORA = "fundo-flora" as const;
+
+export const FRAMEWORK_KEYS_TF = [TERRAFUND, LANDSCAPES, ENTERPRISES, EPA] as const;
+export type FrameworkKeyTF = (typeof FRAMEWORK_KEYS_TF)[number];
+
+export const FRAMEWORK_KEYS = [...FRAMEWORK_KEYS_TF, PPC, HBF, FUNDO_FLORA] as const;
 export type FrameworkKey = (typeof FRAMEWORK_KEYS)[number];
--- libs/database/src/lib/constants/index.ts
+++ libs/database/src/lib/constants/index.ts
@@ -2,4 +2,4 @@ export * from "./polygon-indicators";
 export * from "./polygon-status";
 export * from "./landscapes";
 export * from "./organisations";
-export * from "./framework-tf";
+export * from "./framework";

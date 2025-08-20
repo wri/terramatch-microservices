aab63dd791d76d296d1c53b5e616c17bdd572443
LimberHope <limbermv2@gmail.com>
2025-08-20 11:40:16 -0400

    [TM-2390] update financial report policy

--- libs/common/src/lib/policies/financial-report.policy.ts
+++ libs/common/src/lib/policies/financial-report.policy.ts
@@ -3,7 +3,6 @@ import { FinancialReport } from "@terramatch-microservices/database/entities";
 
 export class FinancialReportPolicy extends UserPermissionsPolicy {
   async addRules() {
-    // Allow all actions for FinancialReport - temporary open access
-    this.builder.can(["read", "delete", "update", "approve", "uploadFiles"], FinancialReport);
+    this.builder.can(["read", "delete"], FinancialReport);
   }
 }

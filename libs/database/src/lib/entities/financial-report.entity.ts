cce7fcfb3e253a03f9ae206232fcdb633b371312
Marcolr-dev <80992287+Marcolr-dev@users.noreply.github.com>
2025-08-21 10:31:00 -0400

    [TM-2390] Fix get machine

--- libs/database/src/lib/entities/financial-report.entity.ts
+++ libs/database/src/lib/entities/financial-report.entity.ts
@@ -17,7 +17,7 @@ import { ReportStatus, ReportStatusStates, statusUpdateSequelizeHook, UpdateRequ
 import { chainScope } from "../util/chain-scope";
 import { FrameworkKey } from "../constants";
 import { JsonColumn } from "../decorators/json-column.decorator";
-import { getStateMachine, StateMachineColumn } from "../util/model-column-state-machine";
+import { StateMachineColumn } from "../util/model-column-state-machine";
 import { Organisation } from "./organisation.entity";
 import { FinancialIndicator } from "./financial-indicator.entity";
 

0bcca6af9106c52e2a9734a8350ce4c66baf480f
LimberHope <limbermv2@gmail.com>
2025-08-22 16:00:12 -0400

    [TM-2390] update unit test

--- libs/common/src/lib/policies/financial-report.policy.spec.ts
+++ libs/common/src/lib/policies/financial-report.policy.spec.ts
@@ -24,8 +24,8 @@ describe("FinancialReportPolicy", () => {
     const ppc = await FinancialReportFactory.create({ frameworkKey: "ppc" });
     const tf = await FinancialReportFactory.create({ frameworkKey: "terrafund" });
     await expectAuthority(service, {
-      can: [[["read", "delete", "update"], ppc]],
-      cannot: [[["read", "delete", "update"], tf]]
+      can: [[["read", "delete"], ppc]],
+      cannot: [[["read", "delete"], tf]]
     });
   });
 
@@ -38,12 +38,25 @@ describe("FinancialReportPolicy", () => {
     const fr1 = await FinancialReportFactory.create({ organisationId: org.id });
     const fr2 = await FinancialReportFactory.create();
     await expectAuthority(service, {
-      can: [[["read", "update", "delete"], fr1]],
-      cannot: [[["read", "update", "delete"], fr2]]
+      can: [[["read", "delete"], fr1]],
+      cannot: [[["read", "delete"], fr2]]
     });
   });
 
-  it("does not allow access without manage-own permission", async () => {
+  it("allows managing all financial reports with reports-manage permission", async () => {
+    mockUserId(123);
+    mockPermissions("reports-manage");
+    const fr1 = await FinancialReportFactory.create();
+    const fr2 = await FinancialReportFactory.create();
+    await expectAuthority(service, {
+      can: [
+        [["read", "delete"], fr1],
+        [["read", "delete"], fr2]
+      ]
+    });
+  });
+
+  it("does not allow access without any permission", async () => {
     const org = await OrganisationFactory.create();
     const user = await UserFactory.create({ organisationId: org.id });
     mockUserId(user.id);
@@ -51,7 +64,6 @@ describe("FinancialReportPolicy", () => {
 
     const financialReport = await FinancialReportFactory.create({ organisationId: org.id });
     await expectCannot(service, "read", financialReport);
-    await expectCannot(service, "update", financialReport);
     await expectCannot(service, "delete", financialReport);
   });
 
@@ -62,7 +74,6 @@ describe("FinancialReportPolicy", () => {
 
     const financialReport = await FinancialReportFactory.create();
     await expectCannot(service, "read", financialReport);
-    await expectCannot(service, "update", financialReport);
     await expectCannot(service, "delete", financialReport);
   });
 });

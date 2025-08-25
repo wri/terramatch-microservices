d0ec20864159f31b8c184758a7e4e2eabc4a85bf
Nathan Curtis <nathan.curtis@gmail.com>
2025-08-25 11:57:58 -0700

    [TM-2325] Remove non-functional uploadFiles check that wasn't being used.

--- libs/common/src/lib/policies/organisation.policy.ts
+++ libs/common/src/lib/policies/organisation.policy.ts
@@ -1,26 +1,11 @@
 import { UserPermissionsPolicy } from "./user-permissions.policy";
 
-import { Organisation, User } from "@terramatch-microservices/database/entities";
+import { Organisation } from "@terramatch-microservices/database/entities";
 
 export class OrganisationPolicy extends UserPermissionsPolicy {
   async addRules() {
     if (this.permissions.includes("users-manage")) {
       this.builder.can("create", Organisation);
     }
-
-    const user = await this.getUser();
-    if (user != null) {
-      this.builder.can("uploadFiles", Organisation, { id: user.organisationId, status: "approved" });
-    }
-  }
-
-  private _user?: User | null;
-  private async getUser() {
-    if (this._user != null) return this._user;
-
-    return (this._user = await User.findOne({
-      where: { id: this.userId },
-      attributes: ["organisationId", "status"]
-    }));
   }
 }

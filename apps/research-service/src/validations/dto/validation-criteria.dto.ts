90a5034019bf7359bd2b16b57ed5b63b34e656bc
JORGE <egrojmonroy@gmail.com>
2025-09-12 13:58:06 -0400

    [TM-2356] remove class validators in dto

--- apps/research-service/src/validations/dto/validation-criteria.dto.ts
+++ apps/research-service/src/validations/dto/validation-criteria.dto.ts
@@ -1,26 +1,22 @@
 import { ApiProperty } from "@nestjs/swagger";
-import { IsBoolean, IsDate, IsNumber, IsOptional, IsObject } from "class-validator";
 
 export class ValidationCriteriaDto {
   @ApiProperty({
     description: "The criteria ID that was validated",
     example: 3
   })
-  @IsNumber()
   criteriaId: number;
 
   @ApiProperty({
     description: "Whether the polygon passed this validation criteria",
     example: true
   })
-  @IsBoolean()
   valid: boolean;
 
   @ApiProperty({
     description: "When this validation was last run",
     example: "2025-02-20T22:01:31Z"
   })
-  @IsDate()
   createdAt: Date;
 
   @ApiProperty({
@@ -29,7 +25,5 @@ export class ValidationCriteriaDto {
     required: false,
     type: Object
   })
-  @IsOptional()
-  @IsObject()
   extraInfo?: object | null;
 }

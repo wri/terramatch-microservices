e3dec09c39fb664ccb497da7b420d1ca6104ea85
Nathan Curtis <nathan.curtis@gmail.com>
2025-10-01 12:11:14 -0700

    [TM-2433] These two were removed from PHP's linked fields, but missed in v3.

--- libs/common/src/lib/linkedFields/configuration/organisation.configuration.ts
+++ libs/common/src/lib/linkedFields/configuration/organisation.configuration.ts
@@ -668,20 +668,6 @@ export const OrganisationConfiguration: LinkedFieldConfiguration = {
       inputType: "treeSpecies",
       collection: "historical-tree-species"
     },
-    "org-leadership-team": {
-      property: "leadershipTeam",
-      label: "Leadership Team",
-      resource: "App\\Http\\Resources\\V2\\LeadershipsResource",
-      inputType: "leaderships",
-      collection: "leadership-team"
-    },
-    "org-core-team-leaders": {
-      property: "coreTeamLeaders",
-      label: "Core Team Leaders",
-      resource: "App\\Http\\Resources\\V2\\LeadershipsResource",
-      inputType: "leaderships",
-      collection: "core-team-leaders"
-    },
     "org-tree-species-restored": {
       property: "treeSpeciesHistorical",
       label: "Tree species restored in landscape",

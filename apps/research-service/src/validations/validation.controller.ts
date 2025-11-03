631814269ab94b70f8910432d64f5019605a4e87
Nathan Curtis <nathan.curtis@gmail.com>
2025-11-03 15:33:21 -0800

    [TM-2629] Simplify boolean expression.

--- apps/research-service/src/validations/validation.controller.ts
+++ apps/research-service/src/validations/validation.controller.ts
@@ -65,7 +65,7 @@ export class ValidationController {
 
     const criteriaId = query.criteriaId != null ? (Number(query.criteriaId) as CriteriaId) : undefined;
 
-    if (criteriaId != null && (criteriaId < 1 || Number.isInteger(criteriaId) === false)) {
+    if (criteriaId != null && (criteriaId < 1 || !Number.isInteger(criteriaId))) {
       throw new BadRequestException("criteriaId must be a valid integer greater than or equal to 1");
     }
 

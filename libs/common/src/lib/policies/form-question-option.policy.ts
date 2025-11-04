69c17da220f07013578a50c072349814b79189fc
Nathan Curtis <nathan.curtis@gmail.com>
2025-11-04 11:58:27 -0800

    [TM-2632] Fix uploading images to form question options.
    Not all forms have a framework key set, so the previous approach wasn't working.

--- libs/common/src/lib/policies/form-question-option.policy.ts
+++ libs/common/src/lib/policies/form-question-option.policy.ts
@@ -1,37 +1,10 @@
 import { UserPermissionsPolicy } from "./user-permissions.policy";
-import { FormQuestionOption, Form, FormQuestion, FormSection } from "@terramatch-microservices/database/entities";
-import { Op } from "sequelize";
+import { FormQuestionOption } from "@terramatch-microservices/database/entities";
 
 export class FormQuestionOptionPolicy extends UserPermissionsPolicy {
   async addRules() {
-    if (this.frameworks.length > 0) {
-      const formQuestion = await FormQuestion.findAll({
-        include: [
-          {
-            model: FormSection,
-            required: true,
-            include: [
-              {
-                model: Form,
-                required: true,
-                where: {
-                  frameworkKey: { [Op.in]: this.frameworks }
-                },
-                attributes: []
-              }
-            ],
-            attributes: []
-          }
-        ],
-        attributes: ["id"]
-      });
-
-      const formQuestionsIds = formQuestion.map(q => q.id);
-      if (formQuestionsIds.length > 0) {
-        this.builder.can(["uploadFiles"], FormQuestionOption, {
-          formQuestionId: { $in: formQuestionsIds }
-        });
-      }
+    if (this.permissions.includes("custom-forms-manage")) {
+      this.builder.can(["uploadFiles"], FormQuestionOption);
     }
   }
 }

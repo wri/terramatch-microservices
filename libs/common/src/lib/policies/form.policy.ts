8efe391ffb5da5f7d4ad12174ae881671e0ec434
LimberHope <limbermv2@gmail.com>
2025-11-06 13:52:54 -0400

    [TM-2532] [TM-2531] restored changes

--- libs/common/src/lib/policies/form.policy.ts
+++ libs/common/src/lib/policies/form.policy.ts
@@ -4,7 +4,9 @@ import { Form } from "@terramatch-microservices/database/entities";
 export class FormPolicy extends UserPermissionsPolicy {
   async addRules() {
     if (this.frameworks?.length > 0) {
-      this.builder.can(["uploadFiles"], Form);
+      this.builder.can(["uploadFiles"], Form, {
+        frameworkKey: { $in: this.frameworks }
+      });
     }
   }
 }

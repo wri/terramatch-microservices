7547719256ee28b0d62ec521d35627f52ea8ac3a
Nathan Curtis <nathan.curtis@gmail.com>
2025-11-24 16:20:53 -0800

    [TM-2626] Event processors can't be request scoped, so they also can't cache things in the request scope.

--- libs/common/src/lib/events/event.processor.ts
+++ libs/common/src/lib/events/event.processor.ts
@@ -5,17 +5,14 @@ import { EventService } from "./event.service";
 export abstract class EventProcessor {
   protected constructor(protected readonly eventService: EventService) {}
 
-  private _authenticatedUser?: User | null;
   async getAuthenticatedUser() {
-    if (this._authenticatedUser != null) return this._authenticatedUser;
-
     const userId = RequestContext.currentContext?.req?.authenticatedUserId as number | null | undefined;
     if (userId == null) return null;
 
-    return (this._authenticatedUser = await User.findOne({
+    return await User.findOne({
       where: { id: userId },
       attributes: ["id", "emailAddress", "firstName", "lastName"]
-    }));
+    });
   }
 
   abstract handle(): Promise<void>;

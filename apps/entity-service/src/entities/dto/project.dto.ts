ef1f4bc864f23c4d64783d9cb85a81df3e85e6ec
Marcolr-dev <marc.antoni.lr@gmail.com>
2025-12-01 11:48:57 -0400

    [TM-2673] Add income generating activities field to project DTO, entity, and configuration

--- apps/entity-service/src/entities/dto/project.dto.ts
+++ apps/entity-service/src/entities/dto/project.dto.ts
@@ -177,6 +177,9 @@ export class ProjectFullDto extends ProjectLightDto {
   @ApiProperty({ nullable: true, type: String, isArray: true })
   restorationStrategy: string[] | null;
 
+  @ApiProperty({ nullable: true, type: String, isArray: true })
+  incomeGeneratingActivities: string[] | null;
+
   @ApiProperty()
   treesPlantedCount: number;
 

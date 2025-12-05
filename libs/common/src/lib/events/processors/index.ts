163d1d0c42ab03a2e8c92a258fed5eab2150174d
Nathan Curtis <nathan.curtis@gmail.com>
2025-12-05 10:19:12 -0800

    [TM-2626] Soft delete all hidden relations when an entity is approved.

--- libs/common/src/lib/events/processors/index.ts
+++ libs/common/src/lib/events/processors/index.ts
@@ -3,7 +3,7 @@ import { FieldsApprovalProcessor } from "./fields.approval-processor";
 import { FundingTypeApprovalProcessor } from "./funding-type.approval-processor";
 import { FinancialIndicatorApprovalProcessor } from "./financial-indicator.approval-processor";
 import { DisturbanceReportEntryApprovalProcessor } from "./disturbance-report-entry.approval-processor";
-import { DemographicApprovalProcessor } from "./demographic.approval-processor";
+import { HiddenRelationsApprovalProcessor } from "./hidden-relations.approval-processor";
 
 // A set of processors that should be run any time an EntityType model moves to approved status.
 export const APPROVAL_PROCESSERS: EntityApprovalProcessor[] = [
@@ -11,5 +11,5 @@ export const APPROVAL_PROCESSERS: EntityApprovalProcessor[] = [
   FundingTypeApprovalProcessor,
   FinancialIndicatorApprovalProcessor,
   DisturbanceReportEntryApprovalProcessor,
-  DemographicApprovalProcessor
+  HiddenRelationsApprovalProcessor
 ];

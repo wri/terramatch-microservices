c0aa8a82aa33027bf3979d290c16a1f5fdc80d5c
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2025-12-09 10:48:41 -0400

    [TM-2711] add clean i18n items

--- libs/common/src/lib/localization/localization.service.ts
+++ libs/common/src/lib/localization/localization.service.ts
@@ -1,17 +1,30 @@
 import { Injectable, NotFoundException } from "@nestjs/common";
-import { I18nItem, I18nTranslation, LocalizationKey } from "@terramatch-microservices/database/entities";
+import {
+  Form,
+  FormOptionListOption,
+  FormQuestion,
+  FormQuestionOption,
+  FormSection,
+  FormTableHeader,
+  FundingProgramme,
+  I18nItem,
+  I18nTranslation,
+  LocalizationKey
+} from "@terramatch-microservices/database/entities";
 import { Op } from "sequelize";
 import { ConfigService } from "@nestjs/config";
 import { ITranslateParams, normalizeLocale, tx, t, createNativeInstance } from "@transifex/native";
 import { Dictionary } from "lodash";
 import { ValidLocale } from "@terramatch-microservices/database/constants/locale";
 import { DRAFT, MODIFIED } from "@terramatch-microservices/database/constants/status";
+import { TMLogger } from "../util/tm-logger";
 
 // A mapping of I18nItem ID to a translated value, or null if no translation is available.
 export type Translations = Record<number, string | null>;
 
 @Injectable()
 export class LocalizationService {
+  private readonly logger = new TMLogger(LocalizationService.name);
   constructor(private readonly configService: ConfigService) {}
 
   async translateKeys(keyMap: Dictionary<string>, locale: ValidLocale, replacements: Dictionary<string> = {}) {
@@ -99,18 +112,15 @@ export class LocalizationService {
       token: this.configService.get("TRANSIFEX_TOKEN"),
       secret: this.configService.get("TRANSIFEX_SECRET")
     });
-    const locales = ["fr_FR" /*, "es_MX", "pt_BR"*/];
+    const locales = ["fr_FR", "es_MX", "pt_BR"];
     for (const locale of locales) {
       const dbLocale = locale.split("_").join("-");
       await tx.fetchTranslations(locale);
-      console.log(`Fetched translations for ${normalizeLocale(locale)}`);
       const translations = await tx.cache.getTranslations(locale);
       const keys = Object.keys(translations);
       for (const key of keys) {
         const translation = translations[key];
-        if (translation == null) continue;
         const i18nItems = await I18nItem.findAll({ where: { hash: key } });
-        if (i18nItems.length === 0) continue;
         for (const i18nItem of i18nItems) {
           const isShort = i18nItem.shortValue != null;
           let i18nTranslation = await I18nTranslation.findOne({
@@ -129,13 +139,16 @@ export class LocalizationService {
           } else {
             i18nTranslation.shortValue = isShort ? translation : null;
             i18nTranslation.longValue = isShort ? null : translation;
+            i18nTranslation.language = dbLocale;
             await i18nTranslation.save();
           }
           i18nItem.status = "translated";
-          await i18nItem.save();
+          await i18nItem.save({
+            hooks: false
+          });
         }
       }
-      console.log(`Finished processing ${keys.length} keys for ${normalizeLocale(locale)}`);
+      this.logger.log(`Finished processing ${keys.length} keys for ${normalizeLocale(locale)}`);
     }
   }
 
@@ -169,4 +182,36 @@ export class LocalizationService {
     }, {} as Record<string, { meta: { character_limit: number; context: string; developer_comment: string; occurrences: string[]; tags: string[] }; string: string }>);
     await tx.pushSource(source);
   }
+
+  cleanOldI18nItems = async () => {
+    const i18nEntitiesColumns = [
+      { entity: Form, attributes: ["titleId", "subtitleId", "descriptionId", "submissionMessageId"] },
+      { entity: FormOptionListOption, attributes: ["labelId"] },
+      { entity: FormQuestion, attributes: ["labelId", "descriptionId"] },
+      { entity: FormQuestionOption, attributes: ["labelId"] },
+      { entity: FormSection, attributes: ["titleId", "subtitleId", "descriptionId"] },
+      { entity: FormTableHeader, attributes: ["labelId"] },
+      { entity: LocalizationKey, attributes: ["valueId"] },
+      { entity: FundingProgramme, attributes: ["locationId"] }
+    ];
+    const i18nIds: number[] = [];
+    for (const { entity, attributes } of i18nEntitiesColumns) {
+      // @ts-expect-error - entity is a model class
+      const entities = await entity.findAll({ attributes });
+      for (const entity of entities) {
+        Object.entries(entity.dataValues).forEach(([_, value]) => {
+          if (value != null) {
+            i18nIds.push(value);
+          }
+        });
+      }
+    }
+    this.logger.log(`i18nIds.length: ${i18nIds.length}`);
+    const i18nItems = await I18nItem.count();
+    this.logger.log(`i18nItems.length: ${i18nItems}`);
+    const translationsDeleted = await I18nTranslation.destroy({ where: { i18nItemId: { [Op.notIn]: i18nIds } } });
+    const itemsDeleted = await I18nItem.destroy({ where: { id: { [Op.notIn]: i18nIds } } });
+    this.logger.log(`Deleted ${itemsDeleted} I18nItems and ${translationsDeleted} I18nTranslations`);
+    this.logger.log(`Finished cleaning old I18nItems`);
+  };
 }

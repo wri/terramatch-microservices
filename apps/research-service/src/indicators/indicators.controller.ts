423e653fb21a8c1a1c2708f722d444624c52eca9
LimberHope <limbermv2@gmail.com>
2025-12-09 11:44:16 -0400

    [TM-2696] add metadata for delayedJob

--- apps/research-service/src/indicators/indicators.controller.ts
+++ apps/research-service/src/indicators/indicators.controller.ts
@@ -3,7 +3,7 @@ import { ApiExtraModels, ApiOperation } from "@nestjs/swagger";
 import { IndicatorTreeCoverDto, IndicatorTreeCoverLossDto } from "../site-polygons/dto/indicators.dto";
 import { InjectQueue } from "@nestjs/bullmq";
 import { Queue } from "bullmq";
-import { DelayedJob } from "@terramatch-microservices/database/entities/delayed-job.entity";
+import { DelayedJob, Site, Project, SitePolygon } from "@terramatch-microservices/database/entities";
 import { JsonApiResponse } from "@terramatch-microservices/common/decorators/json-api-response.decorator";
 import { buildDelayedJobResponse } from "@terramatch-microservices/common/util";
 import { DelayedJobDto } from "@terramatch-microservices/common/dto/delayed-job.dto";
@@ -13,6 +13,9 @@ import { TMLogger } from "@terramatch-microservices/common/util/tm-logger";
 import { IndicatorsParamDto } from "./dto/indicators-param.dto";
 import { IndicatorHectaresDto } from "../site-polygons/dto/indicators.dto";
 import { SitePolygonLightDto } from "../site-polygons/dto/site-polygon.dto";
+import { uniq } from "lodash";
+import { isNotNull } from "@terramatch-microservices/database/types/array";
+import { Op } from "sequelize";
 
 @Controller("research/v3/indicators")
 @ApiExtraModels(IndicatorTreeCoverLossDto, IndicatorHectaresDto)
@@ -33,6 +36,82 @@ export class IndicatorsController {
     @Request() { authenticatedUserId }
   ) {
     this.logger.debug(`Starting indicator calculation for slug: ${slug}`);
+    this.logger.debug(`payload: ${JSON.stringify(payload)}`);
+
+    const { polygonUuids } = payload.data.attributes;
+
+    // Determine entity information from polygons (similar to PolygonClippingController)
+    let entityId: number | undefined;
+    let entityType: string | undefined;
+    let entityName: string;
+
+    if (polygonUuids.length > 0) {
+      const sitePolygons = await SitePolygon.findAll({
+        where: {
+          polygonUuid: {
+            [Op.in]: polygonUuids
+          }
+        },
+        attributes: ["siteUuid"],
+        include: [
+          {
+            association: "site",
+            attributes: ["id", "uuid", "name", "projectId"],
+            include: [
+              {
+                association: "project",
+                attributes: ["id", "uuid", "name"]
+              }
+            ]
+          }
+        ]
+      });
+
+      if (sitePolygons.length > 0) {
+        const uniqueSiteUuids = uniq(sitePolygons.map(({ siteUuid }) => siteUuid).filter(isNotNull));
+
+        if (uniqueSiteUuids.length > 0) {
+          // Extract sites from sitePolygons (they're already loaded with includes)
+          const sites = sitePolygons
+            .map(sp => sp.site)
+            .filter(isNotNull)
+            .filter((site, index, self) => index === self.findIndex(s => s.uuid === site.uuid));
+
+          if (sites.length > 0) {
+            const uniqueProjectIds = new Set(sites.map(s => s.projectId).filter(id => id != null));
+
+            if (uniqueSiteUuids.length === 1) {
+              // All polygons belong to a single site
+              const site = sites[0];
+              entityId = site.id;
+              entityType = Site.LARAVEL_TYPE;
+              entityName = site.name;
+            } else if (uniqueProjectIds.size === 1) {
+              // All polygons belong to different sites but same project
+              const project = sites[0]?.project;
+              if (project != null) {
+                entityId = project.id;
+                entityType = Project.LARAVEL_TYPE;
+                entityName = project.name ?? "Unknown Project";
+              } else {
+                entityName = `${polygonUuids.length} polygons`;
+              }
+            } else {
+              // Polygons belong to multiple sites/projects
+              entityName = `${polygonUuids.length} polygons`;
+            }
+          } else {
+            entityName = `${polygonUuids.length} polygons`;
+          }
+        } else {
+          entityName = `${polygonUuids.length} polygons`;
+        }
+      } else {
+        entityName = `${polygonUuids.length} polygons`;
+      }
+    } else {
+      entityName = "0 polygons";
+    }
 
     const delayedJob = await DelayedJob.create({
       isAcknowledged: false,
@@ -40,7 +119,11 @@ export class IndicatorsController {
       processedContent: 0,
       progressMessage: "Starting indicator calculation...",
       createdBy: authenticatedUserId,
-      metadata: {}
+      metadata: {
+        ...(entityId != null && { entity_id: entityId }),
+        ...(entityType != null && { entity_type: entityType }),
+        entity_name: entityName
+      }
     } as DelayedJob);
 
     await this.indicatorsQueue.add("indicatorCalculation", {

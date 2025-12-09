8eec8127c9c7967c5e6b2d1071b60801d64da77a
Nathan Curtis <nathan.curtis@gmail.com>
2025-12-09 15:07:57 -0800

    [TM-2652] Document the submission get endpoint.

--- apps/entity-service/src/forms/submissions.controller.ts
+++ apps/entity-service/src/forms/submissions.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get, NotFoundException, Param } from "@nestjs/common";
+import { Controller, Get, NotFoundException, Param, UnauthorizedException } from "@nestjs/common";
 import { ApiOperation } from "@nestjs/swagger";
 import { PolicyService } from "@terramatch-microservices/common";
 import { buildJsonApi } from "@terramatch-microservices/common/util";
 import { SubmissionDto } from "../entities/dto/submission.dto";
 import { FormDataService } from "../entities/form-data.service";
 import { SingleResourceDto } from "@terramatch-microservices/common/dto/single-resource.dto";
+import { ExceptionResponse, JsonApiResponse } from "@terramatch-microservices/common/decorators";
 
 @Controller("forms/v3/submissions")
 export class SubmissionsController {
@@ -12,6 +13,9 @@ export class SubmissionsController {
 
   @Get(":uuid")
   @ApiOperation({ operationId: "submissionGet", summary: "Get a single form submission by UUID" })
+  @JsonApiResponse(SubmissionDto)
+  @ExceptionResponse(NotFoundException, { description: "Submission not found" })
+  @ExceptionResponse(UnauthorizedException, { description: "User is not authorized to access this submission" })
   async submissionGet(@Param() { uuid }: SingleResourceDto) {
     const submission = await this.formDataService.getFullSubmission(uuid);
     if (submission == null) throw new NotFoundException("Submission not found");

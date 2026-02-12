8c62a0512b8c03cc336a725dbd9a9bff2f6eaf78
cesarLima1 <cesmaulim@gmail.com>
2026-02-12 19:30:21 -0400

    [TM-2883] add media as sideload for organizations

--- apps/user-service/src/organisations/dto/organisation-show-query.dto.ts
+++ apps/user-service/src/organisations/dto/organisation-show-query.dto.ts
@@ -1,7 +1,7 @@
 import { ApiProperty } from "@nestjs/swagger";
 import { IsArray, IsIn, IsOptional } from "class-validator";
 
-const ORGANISATION_SIDELOADS = ["financialCollection", "financialReport", "cover", "fundingTypes"] as const;
+const ORGANISATION_SIDELOADS = ["financialCollection", "financialReport", "media", "fundingTypes"] as const;
 type OrganisationSideload = (typeof ORGANISATION_SIDELOADS)[number];
 
 export class OrganisationShowQueryDto {

8a30bc4f724bc5ff266785fb9fd17b50665a03e9
Marcolr-dev <marc.antoni.lr@gmail.com>
2026-02-17 16:05:26 -0400

    [TM-2958] add unit tests for TasksService, covering `getTasks` and `getTask` methods.

--- apps/entity-service/src/entities/tasks.service.spec.ts
+++ apps/entity-service/src/entities/tasks.service.spec.ts
@@ -151,11 +151,14 @@ describe("TasksService", () => {
         clock.setSystemTime(newDate);
         tasks[0].setDataValue("status", "approved");
         await tasks[0].save();
+        clock.tick(1000);
         clock.setSystemTime((newDate = DateTime.fromJSDate(newDate).plus({ hours: 1 }).toJSDate()));
         await tasks[1].update({ status: "awaiting-approval" });
+        clock.tick(1000);
         clock.setSystemTime(DateTime.fromJSDate(newDate).plus({ hours: 1 }).toJSDate());
         tasks[2].setDataValue("status", "needs-more-information");
         await tasks[2].save();
+        clock.tick(1000);
 
         const frameworks: string[] = [];
         for (const task of tasks) {

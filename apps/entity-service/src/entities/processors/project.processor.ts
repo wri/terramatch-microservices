96a53e0aec32385abc037b3ece94c705619cb6a4
Nathan Curtis <nathan.curtis@gmail.com>
2026-02-18 14:56:21 -0800

    [TM-2937] WIP update to pitches -> projects mapping.
    Waiting on feedback from Ben on two issues discovered, so putting a pin in this for now.

--- apps/entity-service/src/entities/processors/project.processor.ts
+++ apps/entity-service/src/entities/processors/project.processor.ts
@@ -76,14 +76,14 @@ const PITCH_COPY_ATTRIBUTES: SharedPitchAttributes[] = [
   "proposedNumNurseries",
   "projBoundary",
   "proposedGovPartners",
-  "proposedNumNurseries",
   "states",
   "waterSource",
   "baselineBiodiversity",
   "goalTreesRestoredPlanting",
   "goalTreesRestoredAnr",
   "goalTreesRestoredDirectSeeding",
-  "directSeedingSurvivalRate"
+  "directSeedingSurvivalRate",
+  "landownerAgreement"
 ];
 
 export class ProjectProcessor extends EntityProcessor<
@@ -535,6 +535,11 @@ export class ProjectProcessor extends EntityProcessor<
       attributes.treesGrownGoal = pitch.totalTrees;
       attributes.landTenureProjectArea = pitch.landTenureProjArea;
       attributes.projImpactBiodiv = pitch.biodiversityImpact;
+      // attributes.country = pitch.level0Proposed;
+      attributes.level1Project = pitch.level1Proposed;
+      attributes.level2Project = pitch.level2Proposed;
+      attributes.survivalRate = pitch.projSurvivalRate;
+      attributes.communityEngagementPlan = pitch.landholderCommEngage;
     }
 
     const project = await Project.create(attributes);
@@ -590,7 +595,7 @@ export class ProjectProcessor extends EntityProcessor<
       if (entriesToCreate.length > 0) await TrackingEntry.bulkCreate(entriesToCreate);
 
       const medias = await Media.for(pitch)
-        .collection(["detailed_project_budget", "proof_of_land_tenure_mou"])
+        .collection(["detailed_project_budget", "proof_of_land_tenure_mou", "consortium_partnership_agreements"])
         .findAll();
       await Promise.all(medias.map(media => this.entitiesService.duplicateMedia(media, project)));
     }

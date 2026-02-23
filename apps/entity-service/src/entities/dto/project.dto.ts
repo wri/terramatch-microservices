d251f308147943a62694f5ca9beef21cef872585
LimberHope <limbermv2@gmail.com>
2026-02-23 09:33:25 -0400

    add jobsCreatedGoal for project dto

--- apps/entity-service/src/entities/dto/project.dto.ts
+++ apps/entity-service/src/entities/dto/project.dto.ts
@@ -191,6 +191,9 @@ export class ProjectFullDto extends ProjectLightDto {
   treesGrownGoal: number | null;
 
   @ApiProperty({ nullable: true, type: Number })
+  jobsCreatedGoal: number | null;
+
+  @ApiProperty({ nullable: true, type: Number })
   survivalRate: number | null;
 
   @ApiProperty({ nullable: true, type: Number })

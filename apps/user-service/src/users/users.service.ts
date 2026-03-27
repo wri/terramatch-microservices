da085ededce0741df5717bde3c7bbe5b3aa7b676
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2026-03-26 21:45:46 -0400

    [TM-3071] fix lint

--- apps/user-service/src/users/users.service.ts
+++ apps/user-service/src/users/users.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable, NotFoundException } from "@nestjs/common";
+import { BadRequestException, Injectable } from "@nestjs/common";
 import { Op } from "sequelize";
 import { Framework, FrameworkUser, ModelHasRole, Role, User } from "@terramatch-microservices/database/entities";
 import { PaginatedQueryBuilder } from "@terramatch-microservices/common/util/paginated-query.builder";

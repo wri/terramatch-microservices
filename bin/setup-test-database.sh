6108c69f1e0a4547b6200d82f35ab6378bcb55c6
Nathan Curtis <nathan.curtis@gmail.com>
2026-04-02 14:08:04 -0700

    [TM-3105] Use npx to run nx in this script.

--- bin/setup-test-database.sh
+++ bin/setup-test-database.sh
@@ -7,7 +7,4 @@ grant all on terramatch_microservices_test.* to 'wri'@'%';
 SQL
 
 # Sync the DB schema
-nx test database --no-cloud --skip-nx-cache libs/database/src/lib/database.module.spec.ts
-
-#  	echo "create database if not exists terramatch_test;" | docker compose exec -T mariadb mysql -h localhost -u root -proot
-   #	echo "grant all on terramatch_test.* to 'wri'@'%';" | docker compose exec -T mariadb mysql -h localhost -u root -proot
+npx nx test database --no-cloud --skip-nx-cache libs/database/src/lib/database.module.spec.ts

d7ebc26d8a3354c7ae0cfa1f7bd7d79b2b33ac70
Jose Carlos Laura Ramirez <pachonjcl@gmail.com>
2026-06-29 18:27:30 -0400

    [TM-3576] add force parameter for pulling tranlsations

---
+++ apps/entity-service/src/forms/dto/form-pull-translation-query.dto.ts
@@ -0,0 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { TransformBooleanString } from "@terramatch-microservices/common/decorators/transform-boolean-string.decorator";
+import { IsOptional } from "class-validator";
+
+export class FormPullTranslationQueryDto {
+  @ApiProperty({
+    required: false,
+    default: false,
+    description: "If true, all translations will be pulled, otherwise only new translations will be pulled"
+  })
+  @IsOptional()
+  @TransformBooleanString({ optional: true })
+  forceAll: boolean = false;
+}

a128118da7ed752aab9fb4b99f95b29f3873e44b
Nathan Curtis <nathan.curtis@gmail.com>
2026-09-23 14:11:55 -0700

    [TM-3954] Add rs_tree_count table.

---
+++ libs/database/src/lib/migrations/202609231200-create-rs-tree-count.ts
@@ -0,0 +1,25 @@
+import { BIGINT, DATE, INTEGER, QueryInterface, STRING } from "sequelize";
+import { RunnableMigration } from "umzug";
+
+export const createRsTreeCount: RunnableMigration<QueryInterface> = {
+  name: "202609231200-create-rs-tree-count",
+
+  async up({ context }) {
+    await context.createTable("rs_tree_count", {
+      id: { type: BIGINT.UNSIGNED, allowNull: false, primaryKey: true, autoIncrement: true },
+      created_at: { type: DATE, defaultValue: null },
+      updated_at: { type: DATE, defaultValue: null },
+      deleted_at: { type: DATE, defaultValue: null },
+      project_id: { type: BIGINT.UNSIGNED, allowNull: false },
+      verification_method: { type: STRING, allowNull: false },
+      reported_count: { type: INTEGER, allowNull: false },
+      tree_count_adj: { type: INTEGER, allowNull: false },
+      upper_bounds: { type: INTEGER, allowNull: false },
+      lower_bounds: { type: INTEGER, allowNull: false }
+    });
+  },
+
+  async down({ context }) {
+    await context.dropTable("rs_tree_count");
+  }
+};
